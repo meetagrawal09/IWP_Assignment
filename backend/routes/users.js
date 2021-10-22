50055dd9942756c933648a4e83d1c9d31ed4d6a2
meetagrawal09 <agrawalmeet91@gmail.com>
2021-10-22 21:12:59 +0530

    first commit

---
+++ backend/routes/users.js
@@ -0,0 +1,88 @@
+const { response } = require('express')
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+//getting all users 
+router.get('/',async(req,res)=>{
+    try{
+        const users = await User.find()
+        res.json(users)
+    }catch(err){
+        res.status(500).json({ message:err.message })
+    }
+})
+
+
+//getting one user
+router.get('/:id',getUser,(req,res)=>{
+    res.json(res.user)
+})
+
+
+//Creating one user
+router.post('/',async(req,res)=>{
+    const user = new User({
+        name:req.body.name,
+        email:req.body.email,
+    })
+
+
+    try{
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    }catch(err){
+        res.status(400).json({ message:err.message })
+    }
+})
+
+
+//Updating one
+router.patch('/:id',getUser,async(req,res)=>{
+    if(req.body.name != null){
+        res.user.name = req.body.name
+    }
+    if(req.body.email != null){
+        res.user.email = req.body.email
+    }
+
+    try{
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+
+    } catch(err){
+        res.status(400).json({ message:err.message })
+    }
+})
+
+
+//Deleting one
+router.delete('/:id',getUser,async(req,res)=>{
+    try{
+        await res.user.remove()
+        res.json({ message: 'Deleted User' })
+    }catch(err){
+        res.status(500).json( {message:err.message} )
+    }
+})
+
+
+//middleware function
+async function getUser(req,res,next){
+
+    let user 
+
+    try{
+        user = await User.findById(req.params.id)
+        if(user == null){
+            return res.status(404).json('Cannot find User')
+        }
+    }catch(err){
+        return res.status(500).json({ message:err.message })
+    }
+
+    res.user = user
+    next()
+}
+
+module.exports = router
--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
+
 
 function App() {
+
+  const [update,setUpdate] = useState(false)
+  
+  const [users,setUsers] = useState([])
+
+  const [user,setUser] = useState({})
+
+  useEffect(()=>{
+    getUsers()
+  },[])
+
+  const getUsers=()=>{
+    fetch('http://localhost:4000/users')
+      .then((response)=>response.json())
+      .then((data)=>{
+        setUsers(data);
+        console.log(data)
+      }) 
+      .catch(console.log('ERROR:in getting users')) 
+  }
+
+  const handleChange = (e)=>{
+    const {name,value} = e.target
+    setUser({...user,
+      [name]:value
+    })
+  }
+
+  const handleUpdate = (ele)=>{
+    console.log('update')
+    setUpdate(true)
+    setUser(ele)
+  }
+
+  const handleSubmit = (e)=>{
+      e.preventDefault()
+
+      var url = 'http://localhost:4000/users'
+
+      var method = 'POST'
+
+      if(update === true)
+      {
+          url = `http://localhost:4000/users/${user._id}`
+          setUpdate(false)
+          method = 'PATCH'
+      }
+      
+      fetch(url ,{
+        method: method,
+        headers:{
+            'Content-type':'application/json',
+        },
+        body:JSON.stringify(user)
+      })
+      .then((response)=> {
+          setUser({})
+          console.log('here')
+          console.log('RESPONSE IS:',response)
+          getUsers()
+      })
+      .catch(function(error){
+          console.log('ERROR:',error)
+      })
+  }
+
+  const handleDelete = (ele)=>{
+
+    fetch(`http://localhost:4000/users/${ele._id}`,{
+      method:'DELETE',
+      headers:{
+          'Content-type':'application/json',
+      },
+    })
+    .then((response)=>{
+        console.log(response)
+        getUsers()
+      }
+    )
+    .catch((err)=>console.log(err))
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+
+      {users.map(function (user) {
+         return <div key={user.id}>
+            <p>{user.name}</p>
+            <button onClick={(()=>handleUpdate(user))}>Update</button>
+            <button onClick={(()=>handleDelete(user))}>Delete</button>
+          </div>
+      })}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name = "name" onChange={handleChange} placeholder="Name" value={user?.name}/>
+        <input type="text" name = "email" onChange={handleChange} placeholder="Email" value={user?.email}/>
+        <button>Submit</button>
+      </form>
+
     </div>
   );
 }
